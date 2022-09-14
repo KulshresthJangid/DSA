bab708c7385e8f2d53b02349c4c45915b7f0dd17
KulshresthJangid <71918448+KulshresthJangid@users.noreply.github.com>
2022-09-15 03:20:50 +0530

    Longest Common Prefix in array leetcode

--- src/easy/Arrays.java
+++ src/easy/Arrays.java
@@ -7,7 +7,9 @@ import java.util.*;
 public class Arrays {
 
     public static void main(String[] args) {
-
+        String[] strs = {"flower","flow","flight"};
+        String ans = longestCommonPrefix(strs);
+        System.out.println(ans);
     }
     // 1. Two Sum
     public static int[] twoSum(int[] nums, int target) {
@@ -68,6 +70,19 @@ public class Arrays {
             previous = current;
         }
         return sum;
+    }
+
+    // 14. Longest common prefix
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        for (int i = 0; i < strs[0].length() ; i++){
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j ++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c)
+                    return strs[0].substring(0, i);
+            }
+        }
+        return strs[0];
     }
 
 }
